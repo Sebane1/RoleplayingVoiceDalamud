14bd53859443f01ea43d77dd2c11a40fbb1cf7f2
Sebastina <Sebane1@users.noreply.github.com>
2023-06-25 10:40:48 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,84 @@
+﻿using Dalamud.Game.ClientState;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.Interface.Windowing;
+using Dalamud.Logging;
+using Dalamud.Plugin;
+using DalamudPluginProjectTemplate.Attributes;
+using System;
+
+namespace DalamudPluginProjectTemplate
+{
+    public class Plugin : IDalamudPlugin
+    {
+        private readonly DalamudPluginInterface pluginInterface;
+        private readonly ChatGui chat;
+        private readonly ClientState clientState;
+
+        private readonly PluginCommandManager<Plugin> commandManager;
+        private readonly Configuration config;
+        private readonly WindowSystem windowSystem;
+
+        public string Name => "Your Plugin's Display Name";
+
+        public Plugin(
+            DalamudPluginInterface pi,
+            CommandManager commands,
+            ChatGui chat,
+            ClientState clientState)
+        {
+            this.pluginInterface = pi;
+            this.chat = chat;
+            this.clientState = clientState;
+
+            // Get or create a configuration object
+            this.config = (Configuration)this.pluginInterface.GetPluginConfig()
+                          ?? this.pluginInterface.Create<Configuration>();
+
+            // Initialize the UI
+            this.windowSystem = new WindowSystem(typeof(Plugin).AssemblyQualifiedName);
+
+            var window = this.pluginInterface.Create<PluginWindow>();
+            if (window is not null)
+            {
+                this.windowSystem.AddWindow(window);
+            }
+
+            this.pluginInterface.UiBuilder.Draw += this.windowSystem.Draw;
+
+            // Load all of our commands
+            this.commandManager = new PluginCommandManager<Plugin>(this, commands);
+        }
+
+        [Command("/example1")]
+        [HelpMessage("Example help message.")]
+        public void ExampleCommand1(string command, string args)
+        {
+            // You may want to assign these references to private variables for convenience.
+            // Keep in mind that the local player does not exist until after logging in.
+            var world = this.clientState.LocalPlayer?.CurrentWorld.GameData;
+            this.chat.Print($"Hello, {world?.Name}!");
+            PluginLog.Log("Message sent successfully.");
+        }
+
+        #region IDisposable Support
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+
+            this.commandManager.Dispose();
+
+            this.pluginInterface.SavePluginConfig(this.config);
+
+            this.pluginInterface.UiBuilder.Draw -= this.windowSystem.Draw;
+            this.windowSystem.RemoveAllWindows();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ PluginWindow.cs
@@ -0,0 +1,21 @@
+﻿using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using System.Numerics;
+
+namespace DalamudPluginProjectTemplate
+{
+    public class PluginWindow : Window
+    {
+        public PluginWindow() : base("TemplateWindow")
+        {
+            IsOpen = true;
+            Size = new Vector2(810, 520);
+            SizeCondition = ImGuiCond.FirstUseEver;
+        }
+
+        public override void Draw()
+        {
+            ImGui.Text("Hello, world!");
+        }
+    }
+}
