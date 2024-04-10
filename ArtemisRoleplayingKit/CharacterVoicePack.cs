304e86dc06b77d67a88960cd29ad4cba92b3fcf2
Sebastina <sebane1@thavea.com>
2024-04-10 13:27:37 -0500

    Make random voice clip generation more deterministic between clients.

--- ArtemisRoleplayingKit/CharacterVoicePack.cs
+++ ArtemisRoleplayingKit/CharacterVoicePack.cs
@@ -1,4 +1,5 @@
 ﻿
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,7 +18,6 @@ namespace RoleplayingVoiceDalamud {
         private List<string> _castingAttack = new List<string>();
         private List<string> _castingHeal = new List<string>();
         private Dictionary<string, List<string>> _misc = new Dictionary<string, List<string>>();
-        private Random _random;
         private int emoteIndex;
         private string lastMissed;
         private string lastAction;
@@ -30,15 +30,17 @@ namespace RoleplayingVoiceDalamud {
                     SortFile(file);
                 }
             }
-            _random = new Random();
         }
         public CharacterVoicePack(List<string> files) {
             if (files != null) {
                 foreach (string file in files) {
                     SortFile(file);
                 }
-                _random = new Random();
             }
+        }
+        public unsafe int GetRandom(int min, int max) {
+            Random random = new Random(new TimeSpan(Framework.Instance()->ClientTime.EorzeaTime).Seconds);
+            return random.Next(min, max);
         }
         public void SortFile(string file) {
             if (file.ToLower().EndsWith(".mp3") || file.ToLower().EndsWith(".ogg")) {
@@ -107,7 +109,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetAction(string value) {
             if (_attack.Count > 0 && !value.Contains("sprint") && !value.ToLower().Contains("teleport")) {
-                string action = _attack[_random.Next(0, _attack.Count)];
+                string action = _attack[GetRandom(0, _attack.Count)];
                 if (lastAction != action) {
                     return lastAction = action;
                 }
@@ -119,7 +121,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetMeleeAction(string value) {
             if (_meleeAttack.Count > 0 && !value.Contains("sprint") && !value.ToLower().Contains("teleport")) {
-                string action = _meleeAttack[_random.Next(0, _meleeAttack.Count)];
+                string action = _meleeAttack[GetRandom(0, _meleeAttack.Count)];
                 if (lastAction != action) {
                     return lastAction = action;
                 }
@@ -131,7 +133,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetCastedAction(string value) {
             if (_castedAttack.Count > 0 && !value.Contains("sprint") && !value.ToLower().Contains("teleport")) {
-                string action = _castedAttack[_random.Next(0, _castedAttack.Count)];
+                string action = _castedAttack[GetRandom(0, _castedAttack.Count)];
                 if (lastAction != action) {
                     return lastAction = action;
                 }
@@ -146,7 +148,7 @@ namespace RoleplayingVoiceDalamud {
             string final = !string.IsNullOrWhiteSpace(strippedName) ? strippedName : value;
             foreach (string name in _misc.Keys) {
                 if (final.Contains(name) && name.Length > 4 || final.EndsWith(name)) {
-                    return _misc[name][_random.Next(0, _misc[name].Count)];
+                    return _misc[name][GetRandom(0, _misc[name].Count)];
                 }
             }
             return string.Empty;
@@ -165,14 +167,14 @@ namespace RoleplayingVoiceDalamud {
         }
         public string GetHurt() {
             if (_hurt.Count > 0) {
-                return _hurt[_random.Next(0, _hurt.Count)];
+                return _hurt[GetRandom(0, _hurt.Count)];
             } else {
                 return string.Empty;
             }
         }
         public string GetDeath() {
             if (_death.Count > 0) {
-                return _death[_random.Next(0, _death.Count)];
+                return _death[GetRandom(0, _death.Count)];
             } else {
                 return string.Empty;
             }
@@ -180,7 +182,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetReadying(string value) {
             if (_readying.Count > 0 && !value.ToLower().Contains("teleport")) {
-                return _readying[_random.Next(0, _readying.Count)];
+                return _readying[GetRandom(0, _readying.Count)];
             } else {
                 return string.Empty;
             }
@@ -188,7 +190,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetCastingAttack() {
             if (_castingAttack.Count > 0) {
-                return _castingAttack[_random.Next(0, _castingAttack.Count)];
+                return _castingAttack[GetRandom(0, _castingAttack.Count)];
             } else {
                 return string.Empty;
             }
@@ -196,7 +198,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetCastingHeal() {
             if (_castingHeal.Count > 0) {
-                return _castingHeal[_random.Next(0, _castingHeal.Count)];
+                return _castingHeal[GetRandom(0, _castingHeal.Count)];
             } else {
                 return string.Empty;
             }
@@ -204,7 +206,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetRevive() {
             if (_revive.Count > 0) {
-                return _revive[_random.Next(0, _revive.Count)];
+                return _revive[GetRandom(0, _revive.Count)];
             } else {
                 return string.Empty;
             }
@@ -212,7 +214,7 @@ namespace RoleplayingVoiceDalamud {
 
         public string GetMissed() {
             if (_missed.Count > 0) {
-                string missed = _missed[_random.Next(0, _missed.Count)];
+                string missed = _missed[GetRandom(0, _missed.Count)];
                 if (lastMissed != missed) {
                     return lastMissed = missed;
                 }
