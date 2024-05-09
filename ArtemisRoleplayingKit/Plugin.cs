456d14a68e3797aa4d4eef68cd7dbc94d13d9ddf
Sebastina <sebane1@thavea.com>
2024-05-08 23:54:45 -0500

    Remove call to arms message.

--- ArtemisRoleplayingKit/Plugin.cs
+++ ArtemisRoleplayingKit/Plugin.cs
@@ -378,9 +378,8 @@ namespace RoleplayingVoice {
                 _catalogueWindow.Plugin = this;
                 if (_clientState.IsLoggedIn && !config.NpcSpeechGenerationDisabled) {
                     _chat.Print("Artemis Roleplaying Kit is now using Crowdsourced NPC Dialogue! If you wish to opt out, visit the plugin settings.");
-                    _chat.PrintError("[Artemis Roleplaying Kit] Warning: There are two days left to generate new dialogue until May 9th. We're asking the community to try and play through any quests or content they wouldn't normally play through before the remaining character budget expires.");
+                    _gposeWindow.Initialize();
                 }
-                _gposeWindow.Initialize();
             } catch (Exception e) {
                 _pluginLog.Warning(e, e.Message);
                 _chat.PrintError("[Artemis Roleplaying Kit] Fatal Error, the plugin did not initialize correctly!");
