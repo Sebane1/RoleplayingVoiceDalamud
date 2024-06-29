cabf05c771e1c3ff38b9d65e87f80d4433d43cb6
Sebastina <sebane1@thavea.com>
2024-06-29 18:10:37 -0500

    Move NPC voice data

--- ArtemisRoleplayingKit/Voice/NPCVoiceMapping.cs
+++ ArtemisRoleplayingKit/Voice/NPCVoiceMapping.cs
@@ -1,253 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace RoleplayingVoiceDalamud.Voice {
-    public class NPCVoiceMapping {
+    public static class NPCVoiceMapping {
+        public static NPCVoiceConfiguration _npcVoiceConfiguration;
+        public static void Initialize() {
+            Task.Run(async () => {
+                try {
+                    HttpClient httpClient = new HttpClient();
+                    string json = await httpClient.GetStringAsync(
+                    "https://raw.githubusercontent.com/Sebane1/RoleplayingVoiceDalamud/master/npcVoiceConfiguration.json");
+                    _npcVoiceConfiguration = JsonConvert.DeserializeObject<NPCVoiceConfiguration>(json);
+                } catch (Exception ex) {
+                    if(_npcVoiceConfiguration == null) {
+                        _npcVoiceConfiguration = new NPCVoiceConfiguration();
+                    }
+                }
+            });
+        }
+
+        public static string AliasDetector(string name) {
+            foreach (var key in _npcVoiceConfiguration.NameAndAliasesList.Keys) {
+                foreach (var aliases in _npcVoiceConfiguration.NameAndAliasesList[key]) {
+                    if (aliases.Contains(name)) {
+                        return key;
+                    }
+                }
+            }
+            return name;
+        }
+
         public static Dictionary<string, string> GetVoiceMappings() {
-            Dictionary<string, string> values = new Dictionary<string, string>();
-            values.Add("Alphinaud", "Alev");
-            values.Add("Self-assured Youth", "Alev");
-            values.Add("Alisaie", "Alilev");
-            values.Add("Cynical Girl", "Alilev");
-            values.Add("Haurchefant", "HFort");
-            values.Add("Artoirel", "Afort");
-            values.Add("Charibert", "Cbert");
-            values.Add("Hilda", "Hda");
-            values.Add("Honoroit", "Hroit");
-            values.Add("Ilberd", "Ird");
-            values.Add("Alpha", "Kop");
-            values.Add("King Thordan", "thord");
-            values.Add("Y'shtola", "Yrhul");
-            values.Add("Cultured Conjurer", "Yrhul");
-            values.Add("Thancred", "Twaters");
-            values.Add("Charming Thaumaturge", "Twaters");
-            values.Add("Tataru", "Taru");
-            values.Add("Yda", "Lhext");
-            values.Add("Lyse", "Lhext");
-            values.Add("Animated Pugilist", "Lhext");
-            values.Add("Urianger", "Uauger");
-            values.Add("Masked Man", "Uauger");
-            values.Add("G'raha", "Gia");
-            values.Add("Crystal Exarch", "Gia");
-            values.Add("Minfilia", "Mward");
-            values.Add("Ryne", "Mward");
-            values.Add("Kokkol Dankkol", "Kool");
-            values.Add("Papalymo", "Ptotol");
-            values.Add("Short-tempered Thaumaturge", "Ptotol");
-            values.Add("Krile", "Kmayerbal");
-            values.Add("Matoya", "Moya");
-            values.Add("Hydaelyn", "Hyn");
-            values.Add("Kan-E-Senna", "Kes");
-            values.Add("Merlwyb", "Mbloe");
-            values.Add("Nanamo Ul Namo", "Nulnamo");
-            values.Add("Lilira", "Nulnamo");
-            values.Add("Aymeric", "Adeborel");
-            values.Add("Lucia", "Lcia");
-            values.Add("Taciturn Temple Knight", "Lcia");
-            values.Add("Raubahn", "Rahn");
-            values.Add("Pipin", "Pin");
-            values.Add("Marques", "Cnangarl");
-            values.Add("Cid", "Cnangarl");
-            values.Add("Biggs", "Bman");
-            values.Add("Airship Crewman", "Bman");
-            values.Add("Wedge", "Wman");
-            values.Add("Vidofnir", "Vnir");
-            values.Add("House", "Hfk");
-            values.Add("Warrior of Darkness", "ard");
-            values.Add("Ardbert", "ard");
-            values.Add("Arenvald", "Avald");
-            values.Add("Azemi", "Azmi");
-            values.Add("Bunchin", "Bchin");
-            values.Add("Carvallain", "Cvain");
-            values.Add("Cirina", "Cina");
-            values.Add("Conrad", "Crad");
-            values.Add("Curtis", "Ctis");
-            values.Add("Grynewaht", "Gaht");
-            values.Add("Gyoto", "Goto");
-            values.Add("Hancock", "Hock");
-            values.Add("Ikaruga", "Iruga");
-            values.Add("Hien", "Hen");
-            values.Add("Fordola", "Fola");
-            values.Add("2B", "To be");
-            values.Add("2P", "To be");
-            values.Add("9S", "Nines");
-            values.Add("Isse", "Isse");
-            values.Add("Jifuya", "Juya");
-            values.Add("Lakshmi", "Lhmi");
-            values.Add("Lolorito", "Lito");
-            values.Add("Magnai", "Mai");
-            values.Add("Meffrid", "Mrid");
-            values.Add("M'naago", "Mago");
-            values.Add("Rasho", "Rho");
-            values.Add("Sadu", "Su");
-            values.Add("Shar", "Sar");
-            values.Add("Soroban", "Sban");
-            values.Add("Tansui", "Tui");
-            values.Add("Temulun", "Tun");
-            values.Add("Vira Battlemaid", "Vaid");
-            values.Add("Tsukuyomi", "Tomi");
-            values.Add("Katliss", "Kliss");
-            values.Add("Theyler", "Ter");
-            values.Add("Gaia", "irhm");
-            values.Add("Emet-Selch", "Eselch");
-            values.Add("Hades", "Eselch");
-            values.Add("Almet", "Aet");
-            values.Add("Beq Lugg", "Bug");
-            values.Add("Bragi", "Bi");
-            values.Add("Cassard", "Card");
-            values.Add("Chai-Nuzz", "Cuz");
-            values.Add("Dulia-Chai", "Dai");
-            values.Add("Cymet", "Cet");
-            values.Add("Feo Ul", "Ful");
-            values.Add("Gynard", "Gard");
-            values.Add("Gen Gen", "Geen");
-            values.Add("Guthjon", "Gon");
-            values.Add("Kai-Shirr", "Kir");
-            values.Add("Lyna", "Lna");
-            values.Add("Magnus", "Mus");
-            values.Add("Seto", "So");
-            values.Add("Sul Uin", "Sin");
-            values.Add("Oul Sigun", "Sun");
-            values.Add("Tesleen", "Ten");
-            values.Add("Thaffe", "Tafe");
-            values.Add("Theva", "Tva");
-            values.Add("Uimet", "Uet");
-            values.Add("Wrenden", "Wen");
-            values.Add("Ys Iala", "Yala");
-            values.Add("Hoary Boulder", "Hlder");
-            values.Add("Erenvile", "Eille");
-            values.Add("Fourchenault", "Fault");
-            values.Add("Barnier", "Bier");
-            values.Add("Montichaigne", "Montne");
-            values.Add("Kytte", "Kte");
-            values.Add("Varshahn", "Vahn");
-            values.Add("Nidhana", "Nana");
-            values.Add("Ahewann", "Aann");
-            values.Add("Vrtra", "Vra");
-            values.Add("A-Ruhn-Senna", "Aenna");
-            values.Add("Sicard", "Sard");
-            values.Add("Maxima", "Mima");
-            values.Add("Jullus", "Jlus");
-            values.Add("Quintus", "Qus");
-            values.Add("Livingway", "Lway");
-            values.Add("Mappingway", "Mway");
-            values.Add("Growingway", "Gway");
-            values.Add("Loporrit", "Lrit");
-            values.Add("Matsya", "Mya");
-            values.Add("Mehvan", "Mvan");
-            values.Add("Elidibus", "Ebus");
-            values.Add("Themis", "Ebus");
-            values.Add("White-robed Ascian", "Ebus");
-            values.Add("Hermes", "Hes");
-            values.Add("Meteion", "Mion");
-            values.Add("Hythlodaeus", "Hdaeus");
-            values.Add("Venat", "Vnat");
-            values.Add("Lahabrea", "Lbrea");
-            values.Add("Galene", "Gene");
-            values.Add("Wilfsunn", "Wun");
-            values.Add("Bloewyda", "Byda");
-            values.Add("Moenbryda", "Lhext");
-            values.Add("Eya", "Eya");
-            values.Add("Sir", "techBot");
-            values.Add("Omega", "techBot");
-            values.Add("M-017", "m7");
-            values.Add("Zero", "Zo");
-            values.Add("Wuk Lamat", "Wamat");
-            values.Add("Fandaniel", "Fdaniel");
-            values.Add("Asahi", "Fdaniel");
-            values.Add("Midgardsormr", "Msrmr");
-            values.Add("Worldly Affairs Official", "Wao");
-            values.Add("Ameliance", "Aiance");
-            values.Add("Moogle", "Kop");
-            values.Add("Kuplo Kopp", "Kop");
-            values.Add("Sula", "tf1");
-            values.Add("Sevrin", "Sard");
-            values.Add("Masked Mage", "Lbrea");
-            values.Add("???", "Sys");
-            values.Add("Ifrit", "Rava");
-            values.Add("Varis zos Galvus", "Vzosgal");
-            values.Add("Zenos", "Zyaegal");
-            values.Add("Gaius", "Gvanbael");
-            values.Add("Shadowhunter", "Gvanbael");
-            values.Add("Menacing Mercenary", "Gvanbael");
-            values.Add("Nero", "Ntolscae");
-            values.Add("Runar", "Rar");
-            values.Add("Estinien", "Ewyrm");
-            values.Add("Ysayle", "Ydango");
-            values.Add("Iceheart", "Ydango");
-            values.Add("Count Edmont de Fortemps", "Edefort");
-            values.Add("Emmanellain", "Emmdefort");
-            values.Add("Yugiri", "Ymist");
-            values.Add("Nabriales", "Lbrea");
-            values.Add("Gosetsu", "Gever");
-            values.Add("Yotsuyu", "Ygoeb");
-            values.Add("Radovan", "Rvan");
-            values.Add("Hraesvelgr", "Hralgr");
-
-            values.Add("Byregot", "Bgot");
-            values.Add("Rhalgr", "Rhgr");
-            values.Add("Azeyma", "Azma");
-            values.Add("Nald'thal", "Nathal");
-            values.Add("Menphina", "Mhina");
-            values.Add("Halone", "Hone");
-            values.Add("Nymeia", "Nmea");
-            values.Add("Althyk", "Ahyk");
-            values.Add("Nophica", "Nhica");
-            values.Add("Thaliak", "Thaak");
-            values.Add("Llymlaen", "Llaen");
-            values.Add("Oschon", "Oson");
-            values.Add("Eulogia", "Euia");
-
-
-            return values;
+            return _npcVoiceConfiguration.CharacterToVoiceList;
         }
         public static List<KeyValuePair<string, string>> GetExtrasVoiceMappings() {
-            List<KeyValuePair<string, string>> values = new List<KeyValuePair<string, string>>();
-            values.Add(new KeyValuePair<string, string>("Tattooed Man", "Beggarly"));
-            values.Add(new KeyValuePair<string, string>("Storm Officer", "Beggarly"));
-            values.Add(new KeyValuePair<string, string>("Flame Sergeant", "Beggarly"));
-            values.Add(new KeyValuePair<string, string>("Traitorous Flame Soldier", "Beggarly"));
-            values.Add(new KeyValuePair<string, string>("Riol", "Mciv"));
-            values.Add(new KeyValuePair<string, string>("Titan", "Rava"));
-            values.Add(new KeyValuePair<string, string>("Ramuh", "Rava"));
-            values.Add(new KeyValuePair<string, string>("Garuda", "Hyn"));
-            values.Add(new KeyValuePair<string, string>("Baderon", "Mciv"));
-            values.Add(new KeyValuePair<string, string>("Frixio", "Moya"));
-            values.Add(new KeyValuePair<string, string>("Raya-O-Senna", "Kes"));
-            values.Add(new KeyValuePair<string, string>("Livia sas Junius", "Ygoeb"));
-            values.Add(new KeyValuePair<string, string>("Teledji Adeledji", "Lito"));
-            values.Add(new KeyValuePair<string, string>("Nabriales", "Lbrea"));
-            values.Add(new KeyValuePair<string, string>("Mogloo", "Beggarly"));
-            return values;
+            return _npcVoiceConfiguration.ExtrasVoiceList;
         }
         public static List<KeyValuePair<string, bool>> GetEchoType() {
-            List<KeyValuePair<string, bool>> values = new List<KeyValuePair<string, bool>>();
-            values.Add(new KeyValuePair<string, bool>("Gaius van Baelsar", true));
-            //values.Add(new KeyValuePair<string, bool>("Lahabrea", true));
-            values.Add(new KeyValuePair<string, bool>("Hades", true));
-            values.Add(new KeyValuePair<string, bool>("Rhitahtyn sas Arvina", true));
-            values.Add(new KeyValuePair<string, bool>("Livia sas Junius", true));
-            values.Add(new KeyValuePair<string, bool>("King Thordan", true));
-            //values.Add(new KeyValuePair<string, bool>("Mother Miournne", true));
-            return values;
+            return _npcVoiceConfiguration.EchoValuesList;
         }
         public static List<KeyValuePair<string, float>> GetPitchValues() {
-            List<KeyValuePair<string, float>> values = new List<KeyValuePair<string, float>>();
-            values.Add(new KeyValuePair<string, float>("Gaius van Baelsar", 0.9f));
-            values.Add(new KeyValuePair<string, float>("Rhitahtyn sas Arvina", 0.9f));
-            values.Add(new KeyValuePair<string, float>("Livia sas Junius", 0.9f));
-            //values.Add(new KeyValuePair<string, float>("Lahabrea", 0.97f));
-            values.Add(new KeyValuePair<string, float>("Frixio", 1.1f));
-            values.Add(new KeyValuePair<string, float>("Raya-O-Senna", 1.05f));
-            values.Add(new KeyValuePair<string, float>("Teledji Adeledji", 1.1f));
-            values.Add(new KeyValuePair<string, float>("Titan", 0.8f));
-            values.Add(new KeyValuePair<string, float>("Ramuh", 0.9f));
-            values.Add(new KeyValuePair<string, float>("Hraesvelgr", 0.8f));
-            //values.Add(new KeyValuePair<string, float>("Azeyma", 1.05f));
-            values.Add(new KeyValuePair<string, float>("Hades", 0.9f));
-            values.Add(new KeyValuePair<string, float>("Moenbryda", 0.9f));
-            values.Add(new KeyValuePair<string, float>("Nabriales", 1.05f));
-            values.Add(new KeyValuePair<string, float>("King Thordan", 0.9f));
-            return values;
+            return _npcVoiceConfiguration.PitchValuesList;
         }
     }
 }
