5299b17ff9e1dde99d97fe8a2f34aca764db561a
Sebastina <sebane1@thavea.com>
2025-01-31 17:04:14 -0600

    Cap response retries for custom npcs

--- ArtemisRoleplayingKit/GPT API/GPTRequestSender.cs
+++ ArtemisRoleplayingKit/GPT API/GPTRequestSender.cs
@@ -8,60 +8,46 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace RoleplayingSpeechDalamud.GPT_API
-{
-    public class GPTRequestSender
-    {
-        public async Task<string> GetGPTResponse(string sender, string message, string personality, bool local)
-        {
+namespace RoleplayingSpeechDalamud.GPT_API {
+    public class GPTRequestSender {
+        public async Task<string> GetGPTResponse(string sender, string message, string personality, bool local, int failure = 0) {
             var httpWebRequest = (HttpWebRequest)WebRequest.Create(local ?
                 "http://localhost:5000/api/v1/chat" :
                 "https://ai.hubujubu.com:5696");
             httpWebRequest.ContentType = "application/json";
-            ////if (!local)
-            ////{
-            ////    httpWebRequest.Headers.Add("Authorization: Bearer sk-w6u9o5Mz8GaPj7G31pYtUXVZBFc9sTsoBjTTfKDPK7SGkZFG");
-            ////}
             httpWebRequest.Method = "POST";
 
-            try
-            {
-                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-                {
+            try {
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream())) {
                     string json = JsonConvert.SerializeObject(new GPTRequest(sender, message));
                     streamWriter.Write(json);
                 }
                 var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                {
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream())) {
                     var result = streamReader.ReadToEnd();
                     var response = JsonConvert.DeserializeObject<GPTOpenAIResult>(result);
                     return ResponseCleaner(response.choices[0].text);
                 }
-            }
-            catch
-            {
-                return await GetGPTResponse(sender, message, personality, local);
+            } catch (Exception e) {
+                if (failure < 10) {
+                    return await GetGPTResponse(sender, message, personality, local, failure + 1);
+                } else {
+                    return "";
+                }
             }
         }
 
-        public async Task<string> GetGPTResponse(string sender, string message, string personality)
-        {
+        public async Task<string> GetGPTResponse(string sender, string message, string personality) {
             string value = "";
-            using (ClientWebSocket websocket = new ClientWebSocket())
-            {
+            using (ClientWebSocket websocket = new ClientWebSocket()) {
                 await websocket.ConnectAsync(new System.Uri("ws://localhost:5005/api/v1/stream"), default);
                 string json = JsonConvert.SerializeObject(new GPTRequest(sender, message));
                 await SendString(websocket, json, default);
-                while (true)
-                {
+                while (true) {
                     GPTStreamResponse response = JsonConvert.DeserializeObject<GPTStreamResponse>(await ReadString(websocket));
-                    if (response.@event == "text_stream")
-                    {
+                    if (response.@event == "text_stream") {
                         value += response.text;
-                    }
-                    else if (response.@event == "stream_end")
-                    {
+                    } else if (response.@event == "stream_end") {
                         break;
                     }
                 }
@@ -69,14 +55,11 @@ namespace RoleplayingSpeechDalamud.GPT_API
             }
             return ResponseCleaner(value).Replace("You", personality);
         }
-        public string ResponseCleaner(string response)
-        {
+        public string ResponseCleaner(string response) {
             string[] values = response.Split(". ");
             string newValue = values[0];
-            for (int i = 1; i < values.Length; i++)
-            {
-                if (i < values.Length - 1 || (values[i].EndsWith(".") || values[i].EndsWith("\"")))
-                {
+            for (int i = 1; i < values.Length; i++) {
+                if (i < values.Length - 1 || (values[i].EndsWith(".") || values[i].EndsWith("\""))) {
                     {
                         newValue += ". " + values[i];
                     }
@@ -84,23 +67,19 @@ namespace RoleplayingSpeechDalamud.GPT_API
             }
             return newValue.TrimStart('\n').Split("\n")[0].Split("]")[0].Replace("----", @"shakes ""Appologies, I forget myself sometimes""");
         }
-        public static Task SendString(ClientWebSocket ws, String data, CancellationToken cancellation)
-        {
+        public static Task SendString(ClientWebSocket ws, String data, CancellationToken cancellation) {
             var encoded = Encoding.UTF8.GetBytes(data);
             var buffer = new ArraySegment<Byte>(encoded, 0, encoded.Length);
             return ws.SendAsync(buffer, WebSocketMessageType.Text, true, cancellation);
         }
 
-        public static async Task<String> ReadString(ClientWebSocket ws)
-        {
+        public static async Task<String> ReadString(ClientWebSocket ws) {
             ArraySegment<Byte> buffer = new ArraySegment<byte>(new Byte[8192]);
 
             WebSocketReceiveResult result = null;
 
-            using (var ms = new MemoryStream())
-            {
-                do
-                {
+            using (var ms = new MemoryStream()) {
+                do {
                     result = await ws.ReceiveAsync(buffer, CancellationToken.None);
                     ms.Write(buffer.Array, buffer.Offset, result.Count);
                 }
