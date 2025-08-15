4fca85bc8327c9c7d885c718ad3ee86910247b54
Sebastina <sebane1@thavea.com>
2025-08-15 12:42:38 -0500

    Fix soundfilter signature.

--- ArtemisRoleplayingKit/SoundFilter/Filter.cs
+++ ArtemisRoleplayingKit/SoundFilter/Filter.cs
@@ -21,7 +21,7 @@ namespace SoundFilter {
         private static class Signatures {
             internal const string PlaySpecificSound = "48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 33 F6 8B DA 48 8B F9 0F BA E2 0F";
 
-            internal const string GetResourceSync = "E8 ?? ?? ?? ?? 48 8B D8 8B C7";
+            internal const string GetResourceSync = "E8 ?? ?? ?? ?? 48 8B C8 8B C3 F0 0F C0 81";
             internal const string GetResourceAsync = "E8 ?? ?? ?? ?? 48 8B D8 EB 07 F0 FF 83";
             internal const string LoadSoundFile = "E8 ?? ?? ?? ?? 48 85 C0 75 12 B0 F6";
 
